2cd3c48442e1b04b72398186c1c1d3707aebde17
maj <m.a.jafarzadeh@gmail.com>
2021-12-31 15:48:40 +0330

    chakra-ui installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
