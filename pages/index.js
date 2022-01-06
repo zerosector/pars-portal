49b3a237cc5b8e929500e4d1b9152de13042ede6
maj <m.a.jafarzadeh@gmail.com>
2022-01-06 19:23:35 +0330

    reformat code by prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import Meta from "./components/Meta";
+import Meta from "../components/Meta";
 
 export default function Home() {
   return (
     <>
       <Meta title="درگاه خدمات شرکت پارس" description="درگاه خدمات شرکت پارس" />
-      <h1>Hello</h1>
     </>
   );
 }
