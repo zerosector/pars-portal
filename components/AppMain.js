9af3b8b00b86c427bd3f45bd627a29e39a8c4ec7
maj <m.a.jafarzadeh@gmail.com>
2022-01-07 17:27:43 +0330

    created config file and moved card list from MainApp to config file

--- components/AppMain.js
+++ components/AppMain.js
@@ -1,57 +1,6 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import CardList from "./CardList";
-
-const cards = [
-  {
-    caption: "سامانه گاما",
-    iconName: "todo-list",
-  },
-  {
-    caption: "سامانه ptime",
-    iconName: "clock",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "tool",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "online-shopping",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "money-check",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "user",
-  },
-  {
-    caption: "سامانه ptime",
-    iconName: "user",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "user",
-  },
-  {
-    caption: "سامانfsf s fsه گاما",
-    iconName: "user",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "user",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "user",
-  },
-  {
-    caption: "سامانه گاما",
-    iconName: "user",
-    display: "hidden",
-  },
-];
+import config from "../utils/config";
 
 function AppMain() {
   return (
@@ -63,7 +12,7 @@ function AppMain() {
       <Heading fontSize={"xl"} alignSelf={"stretch"} padding={"1rem 0 0.5rem"}>
         {"خدمات شرکت"}
       </Heading>
-      <CardList cards={cards} />
+      <CardList cards={config.cards} />
     </Flex>
   );
 }
---
+++ utils/config.js
@@ -0,0 +1,55 @@
+const config = {
+  cards: [
+    {
+      caption: "سامانه گاما",
+      iconName: "todo-list",
+    },
+    {
+      caption: "سامانه ptime",
+      iconName: "clock",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "tool",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "online-shopping",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "money-check",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "user",
+    },
+    {
+      caption: "سامانه ptime",
+      iconName: "user",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "user",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "user",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "user",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "user",
+    },
+    {
+      caption: "سامانه گاما",
+      iconName: "user",
+      display: "hidden",
+    },
+  ],
+};
+
+export default config;
